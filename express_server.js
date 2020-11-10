f9114a0ade01e78e5c284a2bfc2d7afe87d63e6b
Paulo Santiago <p.santiago796@gmail.com>
2020-11-10 23:20:02 +0000

    installed cookieParser dependencies, created login and logout events, and updated /urls_new to include templateVars with cookie login information

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -22,18 +24,28 @@ app.get("/urls.json", (req, res) => {
 
 // Template Engine Excercise
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"], 
+    urls: urlDatabase
+  };
   // res.render takes in a .ejs file from views, then a variable to pass into the .ejs file
   res.render("urls_index", templateVars);
 });
 
 // Must be defined GET "/urls/:shortURL" because Express will think that new is a route parameter.
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    username: req.cookies["username"], 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
   if (urlDatabase[req.params.shortURL]) {
     res.render("urls_show", templateVars);
   } else {
@@ -50,7 +62,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
-  console.log(longURL.substring(0,8));
+  // console.log(longURL.substring(0,8));
   // adds the submited url into the urlDatabase with a random string ID
   // checks before if http:// was added or not
   if (longURL.substring(0, 7) === "http://") {
@@ -81,6 +93,17 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  // console.log("trying to logout");
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 const generateRandomString = () => {
   // creates a random alpha-numeric string of 6 characters
   const shortURL = Math.random().toString(36).substring(2, 8);
@@ -90,9 +113,3 @@ const generateRandomString = () => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-/* <h2 class="card-title">Edit</h2>
-<form action="/urls/<%= shortURL %>/edit" method="POST" >
-  <h6 class="card-text">New URL: <input type="text" placeholder="<%= longURL %>"><button>Submit</button>
-  </h6>
-</form> */
